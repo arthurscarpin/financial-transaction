1a9fd0470fd5666e454633b74ef44bb4c5cc74fd
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-07-03 01:13:29 -0300

    feat: Add JPA entity for transactions

--- src/main/java/com/api/financial/model/Transaction.java
+++ src/main/java/com/api/financial/model/Transaction.java
@@ -1,4 +1,49 @@
-package com.api.financial.domain;
+package com.api.financial.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "transacao")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "valor")
+    private BigDecimal value;
+
+    @NotNull
+    @PastOrPresent(message = "A data/hora não pode ser futura")
+    @Column(name = "data_hora", nullable = false)
+    private OffsetDateTime dateTime;
+
+    public Transaction() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public OffsetDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(OffsetDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 }
